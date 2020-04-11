e622772803fbbccd45f1fe2e09fe118246f02ae8
RayMuchammad <m.raynaldi.st@gmail.com>
2020-04-11 20:42:41 +0700

    Update Lesson 3: ListView, Gesture, AppBar

---
+++ lesson_3/lib/utilities/constant.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+final kTittleStyle = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.bold
+);
+
+final appBarStyle = AppBar(
+  backgroundColor: Colors.white,
+  brightness: Brightness.light,
+);
--- lesson_3/lib/views/wordList.dart
+++ lesson_3/lib/views/wordList.dart
@@ -1,20 +1,17 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:lesson3/utilities/constant.dart';
 
 class WordList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("List of words"),
-      ),
       body: Center(
         child: RandomWords(),
       ),
     );
   }
-
 }
 
 class RandomWords extends StatefulWidget {
@@ -25,6 +22,7 @@ class RandomWords extends StatefulWidget {
 class _RandomWordsState extends State<RandomWords> {
   final _suggestion = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
+  final Set<WordPair> _saved = Set<WordPair>();
 
   Widget _buildSugestion() {
     return ListView.builder(
@@ -40,19 +38,72 @@ class _RandomWordsState extends State<RandomWords> {
         });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _buildSugestion(),
-    );
-  }
-
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("List of words", style: kTittleStyle),
+        backgroundColor: Colors.white,
+        brightness: Brightness.light,
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.list),
+              onPressed: _pushSaved,
+              color: Colors.black)
+        ],
+      ),
+      body: _buildSugestion(),
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context)
+        .push(MaterialPageRoute<void>(builder: (BuildContext context) {
+      final tiles = _saved.map((WordPair pair) {
+        return ListTile(
+          title: Text(
+            pair.asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+      });
+      final divided = ListTile.divideTiles(tiles: tiles, context: context).toList();
+
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(" Favorite words", style: kTittleStyle),
+          backgroundColor: Colors.white,
+          brightness: Brightness.light,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back, color: Colors.black),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+        ),
+        body: ListView(children: divided),
+      );
+    }));
   }
 }
